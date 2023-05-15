b97412f9b653eccb7363f69b7e6866001df5b5e9
Omar Diab <me@omardiab.com>
2023-05-15 17:50:23 +0900

    move typescript linting to own overrides

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,22 +2,11 @@ const path = require("path");
 
 /* eslint-env node */
 module.exports = {
-  plugins: ["@typescript-eslint", "unused-imports"],
+  plugins: ["unused-imports"],
   ignorePatterns: [".eslintrc.js", "next.config.js"],
-  extends: [
-    "plugin:@typescript-eslint/recommended",
-    "plugin:@typescript-eslint/recommended-requiring-type-checking",
-    "turbo",
-    "prettier",
-  ],
-  parser: "@typescript-eslint/parser",
-  parserOptions: {
-    tsconfigRootDir: __dirname,
-    project: ["./packages/*/tsconfig.json", "./apps/*/tsconfig.json"],
-  },
+  extends: ["turbo", "prettier"],
   rules: {
     "no-unused-vars": "off",
-    "@typescript-eslint/no-unused-vars": "off",
     "unused-imports/no-unused-imports": "error",
     "unused-imports/no-unused-vars": [
       "warn",
@@ -31,6 +20,26 @@ module.exports = {
   },
   root: true,
   overrides: [
+    {
+      files: ["./**/*.{ts,tsx}"],
+      plugins: ["@typescript-eslint"],
+      extends: [
+        "plugin:@typescript-eslint/recommended",
+        "plugin:@typescript-eslint/recommended-requiring-type-checking",
+      ],
+      parser: "@typescript-eslint/parser",
+      parserOptions: {
+        tsconfigRootDir: __dirname,
+        project: [
+          "./tsconfig.json",
+          "./packages/*/tsconfig.json",
+          "./apps/*/tsconfig.json",
+        ],
+      },
+      rules: {
+        "@typescript-eslint/no-unused-vars": "off",
+      },
+    },
     {
       files: ["apps/web/**/*"],
       extends: ["plugin:@next/next/core-web-vitals"],
