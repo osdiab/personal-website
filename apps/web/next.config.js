56a63cf908aef69567fff031f43594507f20503a
Omar Diab <me@omardiab.com>
2023-05-19 16:39:03 +0900

    add typed routes

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,6 +1,4 @@
-const {
-  createVanillaExtractPlugin
-} = require('@vanilla-extract/next-plugin');
+const { createVanillaExtractPlugin } = require("@vanilla-extract/next-plugin");
 const withVanillaExtract = createVanillaExtractPlugin();
 
 /** @type {import('next').NextConfig} */
@@ -13,6 +11,7 @@ const nextConfig = {
     //
     // https://github.com/vanilla-extract-css/vanilla-extract/issues/929#issuecomment-1538555608
     appDir: true,
+    typedRoutes: true,
   },
 };
 
