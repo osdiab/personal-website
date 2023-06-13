b0edd0a4eddf6944e3709493f82229e97d7ad9b4
Omar Diab <me@omardiab.com>
2023-06-13 15:02:00 +0900

    delete built js files

--- apps/web/src/app/api/health/route.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.GET = void 0;
-var server_1 = require("next/server");
-function GET(_req) {
-    return server_1.NextResponse.json({ success: true }, { status: 200 });
-}
-exports.GET = GET;
