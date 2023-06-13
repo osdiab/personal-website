5783a056819f95f1694a4898b1dcd0c7fc749b4c
Omar Diab <me@omardiab.com>
2023-06-13 21:21:28 +0900

    log on healthcheck

--- apps/web/src/app/api/health/route.ts
+++ apps/web/src/app/api/health/route.ts
@@ -1,5 +1,6 @@
 import { NextRequest, NextResponse } from "next/server";
 
 export function GET(_req: NextRequest) {
+  console.log("Requested", { headers: [_req.headers.entries()] });
   return NextResponse.json({ success: true }, { status: 200 });
 }
