2a0beb79da9754f6a98e9ed29acfbd2091a1ebea
Omar Diab <me@omardiab.com>
2023-06-16 15:49:13 +0900

    hook up codegen to doppler

--- packages/gql-web/codegen.ts
+++ packages/gql-web/codegen.ts
@@ -1,13 +1,18 @@
 import type { CodegenConfig } from "@graphql-codegen/cli";
 
+const host = process.env["HASURA_GRAPHQL_HOST"];
+const port = process.env["HASURA_GRAPHQL_PORT"];
+const adminSecret = process.env["HASURA_GRAPHQL_ADMIN_SECRET"];
+if (!host || !port || !adminSecret) {
+  throw new Error("Missing GraphQL engine connection details or admin secret");
+}
+
 const config: CodegenConfig = {
   overwrite: true,
   schema: [
     {
-      "http://localhost:8002/v1/graphql": {
-        headers: {
-          "X-Hasura-Admin-Secret": "adminsecret",
-        },
+      [`http://${host}:${port}/v1/graphql`]: {
+        headers: { "X-Hasura-Admin-Secret": adminSecret },
       },
     },
   ],
@@ -17,12 +22,7 @@ const config: CodegenConfig = {
     "generated/": {
       preset: "client",
       plugins: [],
-      config: {
-        useTypeImports: true,
-        scalars: {
-          uuid: "string",
-        },
-      },
+      config: { useTypeImports: true, scalars: { uuid: "string" } },
       presetConfig: {
         // recommended by graphql-codegen since it's not actually a react hook
         // https://the-guild.dev/graphql/codegen/plugins/presets/preset-client#the-usefragment-helper
