0b28a189382de4caae9ecd62a018649dc0f3006f
Omar Diab <me@omardiab.com>
2023-06-20 23:19:11 +0900

    shorter error

--- packages/zod-schemas/base64-url-uuid.ts
+++ packages/zod-schemas/base64-url-uuid.ts
@@ -4,10 +4,7 @@ import { base64UrlToUuid } from "@osdiab-website/base64-url-uuid";
 export const base64UrlUuidSchema = z
   .string()
   .transform(async (uuid) => await base64UrlToUuid(uuid))
-  .refine(
-    (v) => z.string().uuid().safeParse(v).success,
-    "Not a valid encoded UUID"
-  );
+  .refine((v) => z.string().uuid().safeParse(v).success, "Invalid UUID");
 
 export const uuidOrBase64UrlUuidSchema = z.union([
   z.string().uuid(),
