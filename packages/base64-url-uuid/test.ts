cf9b6a8054048030be969d9e95bcc6db74213040
Omar Diab <me@omardiab.com>
2023-06-20 17:10:50 +0900

    library for making uuids shorter

---
+++ packages/base64-url-uuid/test.ts
@@ -0,0 +1,25 @@
+import fc from "fast-check";
+import { test } from "vitest";
+import { base64UrlToUuid, uuidToBase64Url } from ".";
+
+test("Encodes and decodes consistently", async () => {
+  await fc.assert(
+    fc.asyncProperty(
+      fc.uuid(),
+      async (uuidString) =>
+        uuidString ===
+        (await base64UrlToUuid(await uuidToBase64Url(uuidString)))
+    ),
+    { numRuns: 10_000 }
+  );
+});
+
+test("Results in shorter encoding", async () => {
+  await fc.assert(
+    fc.asyncProperty(
+      fc.uuid(),
+      async (uuidString) =>
+        uuidString.length > (await uuidToBase64Url(uuidString)).length
+    )
+  );
+});
