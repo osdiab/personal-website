5fbc851274286490521d3481cd0de81e8df4ec1d
Omar Diab <me@omardiab.com>
2023-07-21 17:21:05 +0900

    autoprefix, layers support for older ios

--- apps/web/postcss.config.cjs
+++ apps/web/postcss.config.cjs
@@ -1,5 +1,7 @@
 module.exports = {
   plugins: {
-    '@pandacss/dev/postcss': {},
+    "@pandacss/dev/postcss": {},
+    "@csstools/postcss-cascade-layers": {}, // this can be removed maybe in a year
+    autoprefixer: {},
   },
-}
+};
