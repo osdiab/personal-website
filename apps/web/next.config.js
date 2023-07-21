442101aff000d4e87116cf618070c3d11e7d3ec6
Omar Diab <me@omardiab.com>
2023-07-21 10:52:02 +0900

    get rid of vanilla extract next config

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,6 +1,4 @@
-const { createVanillaExtractPlugin } = require("@vanilla-extract/next-plugin");
 const packageJson = require("./package.json");
-const withVanillaExtract = createVanillaExtractPlugin();
 
 const referencedPackageNames = Object.keys(packageJson.dependencies).filter(
   (v) => v.startsWith("@osdiab-website/")
@@ -10,18 +8,8 @@ const referencedPackageNames = Object.keys(packageJson.dependencies).filter(
 const nextConfig = {
   reactStrictMode: true,
   transpilePackages: referencedPackageNames,
-  experimental: {
-    // The App Router is stable, but the Vanilla Extract Next Plugin
-    // currently requires that `experimental.appDir` is `true`.
-    //
-    // https://github.com/vanilla-extract-css/vanilla-extract/issues/929#issuecomment-1538555608
-    appDir: true,
-    typedRoutes: true,
-  },
+  experimental: { typedRoutes: true },
   webpack(config) {
-    // https://github.com/vanilla-extract-css/vanilla-extract/issues/1085#issuecomment-1555986222
-    config.optimization.splitChunks = false;
-
     config.module.rules.push({
       test: /\.svg$/i,
       use: [
@@ -44,4 +32,4 @@ const nextConfig = {
   typescript: { ignoreBuildErrors: true },
 };
 
-module.exports = withVanillaExtract(nextConfig);
+module.exports = nextConfig;
