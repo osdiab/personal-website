d36c5a45b4817c22ce9e2c2b71abbe9664dc9bcc
Omar Diab <me@omardiab.com>
2023-07-21 10:52:12 +0900

    remove libs/ui package

--- libs/ui/card.css.ts
+++
@@ -1,6 +0,0 @@
-import { style } from "@vanilla-extract/css";
-
-export const cardCss = style({
-  border: "1px solid black",
-  borderRadius: "4px",
-});
--- libs/ui/hyperlink.css.ts
+++
@@ -1,7 +0,0 @@
-import { StyleRule, style } from "@vanilla-extract/css";
-
-export const hyperlinkStyles: StyleRule = {
-  color: "blue",
-  textDecoration: "underline",
-};
-export const hyperlinkCss = style(hyperlinkStyles);
