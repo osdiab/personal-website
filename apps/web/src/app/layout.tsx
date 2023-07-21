c864e39b92e6752ffd49fd4efc1d45af757b83d3
Omar Diab <me@omardiab.com>
2023-07-21 10:46:44 +0900

    remove test ui

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -8,7 +8,6 @@ import {
 } from "~/app/layout.css";
 
 import "~/app/global.css";
-import { hstack } from "~pandacss/patterns";
 
 export default function RootLayout({
   children,
@@ -19,11 +18,6 @@ export default function RootLayout({
     <html lang="en">
       <body className={bodyCss}>
         <RootHeader />
-        <div className={hstack()}>
-          <span>Hi</span>
-          <span>Hi</span>
-          <span>Hi</span>
-        </div>
         <main className={mainContentCss}>{children}</main>
         <footer className={footerCss}>
           <div className={footerWrapperCss}>
