f0ba7bf9cd86b7f5b45637929c6135e37ce7ec4b
Omar Diab <me@omardiab.com>
2023-07-21 17:29:40 +0900

    require css cascade layers

--- apps/web/postcss.config.cjs
+++ apps/web/postcss.config.cjs
@@ -1,7 +1,6 @@
 module.exports = {
   plugins: {
     "@pandacss/dev/postcss": {},
-    "@csstools/postcss-cascade-layers": {}, // this can be removed maybe in a year
     autoprefixer: {},
   },
 };
