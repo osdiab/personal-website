e6d0d6ea3b4f14f13911dbd4f6dc09266aa0c3ac
Omar Diab <me@omardiab.com>
2023-07-23 15:48:48 +0900

    comment the wait on theme component

--- apps/web/src/components/wait-on-theme.tsx
+++ apps/web/src/components/wait-on-theme.tsx
@@ -1,11 +1,17 @@
 "use client";
 
-import type { PropsWithChildren} from "react";
+import type { PropsWithChildren } from "react";
 import { useEffect, useState } from "react";
 
 interface WaitOnThemeProps {
   loadingDisplay?: React.ReactNode;
 }
+
+/**
+ * Helper component to wrap around components that depend on the theme, since
+ * not doing so will cause hydration errors between the server and client
+ * @see https://github.com/pacocoursey/next-themes#avoid-hydration-mismatch
+ */
 export function WaitOnTheme({
   loadingDisplay,
   children,
