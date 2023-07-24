53fb9312dbd78aa1b26ac032b2672658c0450929
Omar Diab <me@omardiab.com>
2023-07-24 14:35:36 +0900

    fix animation flicker

--- apps/web/panda.config.ts
+++ apps/web/panda.config.ts
@@ -56,8 +56,8 @@ export default defineConfig({
           "100%": { opacity: 1 },
         },
         fadeOut: {
-          "0%": { opacity: 0 },
-          "100%": { opacity: 1 },
+          "0%": { opacity: 1 },
+          "100%": { opacity: 0 },
         },
       },
       semanticTokens: defineSemanticTokens({
