5c50b0ab0c1b8cca6b359900cf35b5bd289fe9c6
Omar Diab <me@omardiab.com>
2024-04-20 23:23:46 +0900

    just get rid of web next

--- apps/web-next/src/app/api/health/route.ts
+++
@@ -1,6 +0,0 @@
-import type { NextRequest } from "next/server";
-import { NextResponse } from "next/server";
-
-export function GET(_request: NextRequest) {
-	return NextResponse.json({ success: true }, { status: 200 });
-}
--- apps/web-next/src/utils/css-variables.ts
+++
@@ -1,5 +0,0 @@
-export const primaryColorVariables = {
-	hue: "--color-primary-hue",
-	saturation: "--color-primary-saturation",
-	lightness: "--color-primary-lightness",
-} as const;
