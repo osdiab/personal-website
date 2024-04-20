c75be8315ccf9ebd13a3dbd0ad6448e95692729d
Omar Diab <me@omardiab.com>
2024-04-21 00:00:45 +0900

    config eslint

---
+++ .eslintrc.js
@@ -0,0 +1,32 @@
+module.exports = {
+	root: true,
+	env: { es2021: true, node: true },
+	ignorePatterns: [".eslintrc.cjs", "generated/", "build/", "gen/"],
+	rules: {},
+	overrides: [
+		{
+			files: ["./**/*.ts{,x}"],
+			plugins: ["@typescript-eslint"],
+			parser: "@typescript-eslint/parser",
+			parserOptions: {
+				EXPERIMENTAL_useProjectService: true,
+				tsconfigRootDir: __dirname,
+				project: [
+					"./tsconfig.json",
+					"./apps/*/tsconfig.json",
+					"./libs/*/tsconfig.json",
+					"./scripts/*/tsconfig.json",
+				],
+				ecmaVersion: 2021,
+				sourceType: "module",
+				ecmaFeatures: { jsx: true },
+			},
+		},
+		{
+			files: ["./apps/web-app"],
+			extends: ["plugin:qwik/recommended"],
+			env: { browser: true },
+			rules: {},
+		},
+	],
+};
