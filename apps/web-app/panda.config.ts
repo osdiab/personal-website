a32fd19f655b309cef1ef6aab940b61daa8404b8
Omar Diab <me@omardiab.com>
2024-06-10 17:38:04 -0500

    set up color theming conditions, add some more color tokens

--- apps/web-app/panda.config.ts
+++ apps/web-app/panda.config.ts
@@ -3,7 +3,138 @@ import {
 	defineGlobalStyles,
 	defineTextStyles,
 } from "@pandacss/dev";
+import type { Config } from "@pandacss/dev";
+import * as R from "ramda";
+import { rootThemeAttribute } from "~/components/theme-switcher/constants";
 
+type Theme = Config["theme"];
+
+type NestedObject = {
+	[key: string]: string | NestedObject;
+};
+
+/**
+ * Duplicates the _light and _dark values in the theme object to _osLight and
+ * _osDark (recursively) so that the prefers-color-scheme preference behavs as
+ * expected
+ */
+function addOsColorThemes(theme: NestedObject): NestedObject {
+	if (!theme || typeof theme !== "object") {
+		return theme;
+	}
+	const entries = Object.entries(theme);
+	const additionalEntries = [
+		"_light" in theme && typeof theme["_light"] === "string"
+			? ["_osLight", theme["_light"]]
+			: null,
+		"_dark" in theme && typeof theme["_dark"] === "string"
+			? ["_osDark", theme["_dark"]]
+			: null,
+	].filter(R.isNotNil);
+	return Object.fromEntries([
+		...additionalEntries,
+		...entries.map(([key, value]) => [
+			key,
+			addOsColorThemes(value as NestedObject),
+		]),
+	]);
+}
+
+const brandColor = "#F6490D";
+
+const theme: Theme = {
+	extend: {
+		textStyles: defineTextStyles({
+			p: { value: { fontSize: "sm", lineHeight: "1.5em" } },
+			h1: {
+				value: {
+					fontSize: "2xl",
+					lineHeight: "1.2em",
+					fontWeight: "semibold",
+				},
+			},
+			h2: {
+				value: { fontSize: "2xl", lineHeight: "1.2em", fontWeight: "normal" },
+			},
+			h3: {
+				value: {
+					fontSize: "xl",
+					lineHeight: "1.3em",
+					fontWeight: "semibold",
+				},
+			},
+			h4: {
+				value: { fontSize: "xl", lineHeight: "1.3em", fontWeight: "normal" },
+			},
+			h5: {
+				value: {
+					fontSize: "lg",
+					lineHeight: "1.3em",
+					fontWeight: "semibold",
+				},
+			},
+			h6: {
+				value: {
+					fontSize: "md",
+					lineHeight: "1.4em",
+					fontWeight: "semibold",
+				},
+			},
+		}),
+		semanticTokens: {
+			durations: { interactive: { value: "0.1s" } },
+			colors: {
+				brand: { value: brandColor },
+				background: {
+					page: {
+						value: {
+							_light: "#FFFFFF",
+							_dark: "{colors.slate.900}",
+						},
+					},
+					tooltip: {
+						value: {
+							_light: "{colors.slate.100}",
+							_dark: "{colors.slate.800}",
+						},
+					},
+					select: {
+						highlight: {
+							value: {
+								_light: "blue",
+								_dark: "green",
+							},
+						},
+					},
+				},
+				text: {
+					body: {
+						value: {
+							_light: "{colors.slate.950}",
+							_dark: "{colors.slate.50}",
+						},
+					},
+					soft: {
+						value: {
+							_light: "{colors.slate.600}",
+							_dark: "{colors.slate.400}",
+						},
+					},
+					primary: { value: "#F6490D" },
+					primaryHighlight: { value: "#FF7D50" },
+				},
+				border: {
+					soft: {
+						value: {
+							_light: "{colors.slate.200}",
+							_dark: "{colors.slate.700}",
+						},
+					},
+				},
+			},
+		},
+	},
+};
 export default defineConfig({
 	jsxFramework: "qwik",
 
@@ -16,92 +147,24 @@ export default defineConfig({
 	// Files to exclude
 	exclude: [],
 
-	globalCss: defineGlobalStyles({ html: { color: "text.body" } }),
-
-	// Useful for theme customization
-	theme: {
+	conditions: {
 		extend: {
-			textStyles: defineTextStyles({
-				p: { value: { fontSize: "sm", lineHeight: "1.5em" } },
-				h1: {
-					value: {
-						fontSize: "2xl",
-						lineHeight: "1.2em",
-						fontWeight: "semibold",
-					},
-				},
-				h2: {
-					value: { fontSize: "2xl", lineHeight: "1.2em", fontWeight: "normal" },
-				},
-				h3: {
-					value: {
-						fontSize: "xl",
-						lineHeight: "1.3em",
-						fontWeight: "semibold",
-					},
-				},
-				h4: {
-					value: { fontSize: "xl", lineHeight: "1.3em", fontWeight: "normal" },
-				},
-				h5: {
-					value: {
-						fontSize: "lg",
-						lineHeight: "1.3em",
-						fontWeight: "semibold",
-					},
-				},
-				h6: {
-					value: {
-						fontSize: "md",
-						lineHeight: "1.4em",
-						fontWeight: "semibold",
-					},
-				},
-			}),
-			semanticTokens: {
-				durations: { interactive: { value: "0.1s" } },
-				colors: {
-					brand: { value: "#F6490D" },
-					background: {
-						page: {
-							value: {
-								_light: "#FFFFFF",
-								_dark: "{colors.slate.900}",
-							},
-						},
-					},
-					text: {
-						body: {
-							value: {
-								_light: "{colors.slate.950}",
-								_dark: "{colors.slate.50}",
-							},
-						},
-						soft: {
-							value: {
-								_light: "{colors.slate.600}",
-								_dark: "{colors.slate.400}",
-							},
-						},
-						primary: { value: "#F6490D" },
-						primaryHighlight: { value: "#FF7D50" },
-					},
-					border: {
-						soft: {
-							value: {
-								_light: "{colors.slate.200}",
-								_dark: "{colors.slate.700}",
-							},
-						},
-					},
-				},
-			},
+			light: `[${rootThemeAttribute}=light] &`,
+			dark: `[${rootThemeAttribute}=dark] &`,
+			osLight: [
+				`:not([${rootThemeAttribute}]) &`,
+				"@media (prefers-color-scheme: light)",
+			],
+			osDark: [
+				`:not([${rootThemeAttribute}]) &`,
+				"@media (prefers-color-scheme: dark)",
+			],
 		},
 	},
-	conditions: {
-		light: "[data-color-mode=light] &",
-		dark: "[data-color-mode=dark] &",
-	},
+
+	// Useful for theme customization
+	theme: addOsColorThemes(theme as NestedObject) as Theme,
+
 	patterns: {
 		extend: {
 			invisible: {
