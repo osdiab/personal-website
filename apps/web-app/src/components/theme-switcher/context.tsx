93179b5ae60e1c23fc821f75541b5b4a7778e332
Omar Diab <me@omardiab.com>
2024-06-11 11:00:13 -0400

    delete theme context

--- apps/web-app/src/components/theme-switcher/context.tsx
+++
@@ -1,4 +0,0 @@
-import { type Signal, createContextId } from "@builder.io/qwik";
-import type { ThemeOption } from "~/components/theme-switcher/constants";
-
-export const ThemeContext = createContextId<Signal<ThemeOption>>("theme");
